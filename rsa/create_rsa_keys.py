078b807949b348eb6963565f8398cba7ffb75676
pawbu <pawbu@users.noreply.github.com>
2023-05-16 14:49:58 +0200

    ruff-and-black: blacken the code

--- rsa/create_rsa_keys.py
+++ rsa/create_rsa_keys.py
@@ -11,8 +11,13 @@ from cryptography.hazmat.primitives.asymmetric import rsa
     Script to create public and private rsa keys.
 """
 
-parser = argparse.ArgumentParser(description='Generate RSA keys')
-parser.add_argument('-r', '--rsa_key_dir', help='The directory to place the generated keys.', required=True)
+parser = argparse.ArgumentParser(description="Generate RSA keys")
+parser.add_argument(
+    "-r",
+    "--rsa_key_dir",
+    help="The directory to place the generated keys.",
+    required=True,
+)
 
 args = parser.parse_args()
 
@@ -21,9 +26,7 @@ if not os.path.exists(rsa_key_dir):
     os.makedirs(rsa_key_dir)
 
 private_key = rsa.generate_private_key(
-    public_exponent=65537,
-    key_size=2048,
-    backend=default_backend()
+    public_exponent=65537, key_size=2048, backend=default_backend()
 )
 
 public_key = private_key.public_key()
@@ -31,22 +34,22 @@ public_key = private_key.public_key()
 microdata_private_key_pem = private_key.private_bytes(
     encoding=serialization.Encoding.PEM,
     format=serialization.PrivateFormat.PKCS8,
-    encryption_algorithm=serialization.NoEncryption()
+    encryption_algorithm=serialization.NoEncryption(),
 )
 
-private_key_location = f'{rsa_key_dir}/microdata_private_key.pem'
-with open(private_key_location, 'wb') as f:
+private_key_location = f"{rsa_key_dir}/microdata_private_key.pem"
+with open(private_key_location, "wb") as f:
     f.write(microdata_private_key_pem)
 
-print(f'Stored {private_key_location}')
+print(f"Stored {private_key_location}")
 
 microdata_public_key_pem = public_key.public_bytes(
     encoding=serialization.Encoding.PEM,
-    format=serialization.PublicFormat.SubjectPublicKeyInfo
+    format=serialization.PublicFormat.SubjectPublicKeyInfo,
 )
 
-public_key_location = f'{rsa_key_dir}/microdata_public_key.pem'
-with open(f'{rsa_key_dir}/microdata_public_key.pem', 'wb') as f:
+public_key_location = f"{rsa_key_dir}/microdata_public_key.pem"
+with open(f"{rsa_key_dir}/microdata_public_key.pem", "wb") as f:
     f.write(microdata_public_key_pem)
 
-print(f'Stored {public_key_location}')
+print(f"Stored {public_key_location}")
